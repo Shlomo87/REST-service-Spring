74f89ffa55e7281369e6fa8fe877749b5761497c
Kirill Levshin <klevshin@mail.ru>
2016-06-14 14:47:51 +0300

    'mock5'

--- src/test/groovy/mockingTests/DriveSubscriberMock.groovy
+++ src/test/groovy/mockingTests/DriveSubscriberMock.groovy
@@ -1,8 +1,11 @@
+package mockingTests
+
+import hello.DrivePublisher
+import hello.DriveSubscriber
+
 /**
  * Created by klevshin on 6/14/2016.
  */
-
-
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -16,8 +19,28 @@ import org.springframework.test.context.ContextHierarchy;
 import spock.lang.*
 
 @ContextConfiguration( value = "/test/resources/publisher-subscriber-context.xml")
-class DriveSubscriberMock  {
+class DriveSubscriberMock extends Specification {
 
+    @Autowired
+    DrivePublisher drivePublisher
+
+    @Autowired
+    DriveSubscriber driveSubscriber
+
+
+
+
+
+    def "MockingTest"() {
+
+        drivePublisher.subscribers
+
+        String s = "Infiniti"
+
+        expect:
+        s.equals("Infiniti")
+
+    }
 
 
 }
